9e8bcef33c6fd75d51f2ea89575e4d6f83af5154
A <awongpro7@gmail.com>
2021-04-09 03:44:03 +0000

    Refactored and created mocha chai unit test

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-// BREEDFETCHER FUNCTION
+/* BREEDFETCHER FUNCTION 1.0
 
 const breedFetcher = function() {
   const breedName = process.argv.slice(2);
@@ -23,3 +23,19 @@ const breedFetcher = function() {
 };
 
 breedFetcher();
+
+*/
+
+// BREEDFETCHER FUNCTION 2.0
+
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    // if (error) {
+    //   console.log("There was this error:", error);
+    // }
+    const data = JSON.parse(body);
+    // console.log("Description:", data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,15 @@
+// REQUIRE
+
+const { fetchBreedDescription } = require('./breedFetcher');
+
+// INDEX FUNCTION
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log("Error fetch details:", error);
+  } else {
+    console.log(desc);
+  }
+});
